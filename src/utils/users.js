165f130a719fb9428bee1a624adf51cd8e0bd755
NaeNate <naet@qstreet.org>
2020-04-11 22:02:56 -0400

    Make Dropdown for rooms

--- src/utils/users.js
+++ src/utils/users.js
@@ -3,18 +3,12 @@ const users = []
 const addUser = ({ id, username, room }) => {
     //Clean Data
     username = username.trim().toLowerCase()
-    // room = room.trim().toLowerCase()
+    room = room.trim().toLowerCase()
 
-    //validate data
-    // if (!username || !room) {
-    //     return {
-    //         error: 'Username and room are required'
-    //     }
-    // }
-
-    if (!username) {
+    // validate data
+    if (!username || !room) {
         return {
-            error: 'A username is required.'
+            error: 'Username and room are required'
         }
     }
 
@@ -54,9 +48,28 @@ const getRoomUsers = (room) => {
     return users.filter((user) => user.room === room)
 }
 
+const hadDuplicates = (arr) => {
+    return new Set(arr).size !== arr.length
+}
+
+const getRooms = () => {
+    const rooms = []
+    users.forEach((user) => {
+        rooms.push(user.room)
+        if (hadDuplicates(rooms)) {
+            rooms.pop()
+            return rooms
+        } else {
+            return rooms
+        }
+    })
+    return rooms
+}
+
 module.exports = {
     addUser,
     removeUser,
     getRoomUsers,
-    getUser
+    getUser,
+    getRooms
 }
